d8fd08c8b1503eab613fb9a295e6878a511b2e8c
Georgi Gaydov <gaidov@gmail.com>
2020-02-11 20:46:26 +0000

    Add unit 1 exercises

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define(
+	[
+		"sap/ui/core/mvc/Controller",
+		"sap/m/MessageToast"
+	],
+	function (Controller, MessageToast) {
+		"use strict";
+
+		return Controller.extend("opensap.myapp.controller.App", {
+
+			onSayHelloBtnPress: function () {
+				var oBundle = this.getView().getModel("i18n").getResourceBundle();
+				var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+				var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+				MessageToast.show(sMsg);
+			}
+		});
+	});
